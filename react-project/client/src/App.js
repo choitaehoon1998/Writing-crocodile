1a816c084c8a65c32d71dd5a71270e44473ae5d8
choitaehoon1998 <hugebigdream@gmail.com>
2019-12-10 22:47:32 +0900

    for today commit

--- react-project/client/src/App.js
+++ react-project/client/src/App.js
@@ -21,34 +21,26 @@ const styles = theme => ({
 
 })
 
-const customers =[
-  {
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' :'최태훈',
-  'birthday' : '980124',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' :'나동빈',
-  'birthday' : '980124',
-  'gender' : '남자',
-  'job' : '프로그래머'
-},
-{
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' :'허인볌',
-  'birthday' : '980124',
-  'gender' : '남자',
-  'job' : '디자이너'
-}
-]
+
 
 class App extends Component {
+
+  state={
+    customers: ""
+  }
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+
+  }
+
   render(){
     const { classes } = this.props;
 
@@ -66,7 +58,7 @@ class App extends Component {
           </TableRow>
         </TableHead>
         <TableBody>
-        {customers.map(c=> {
+        {this.state.customers ? this.state.customers.map(c=> {
           return(<Customer
             key={c.id}
             id={c.id}
@@ -75,7 +67,7 @@ class App extends Component {
             birthday={c.birthday}
             gender={c.gender}
             job={c.job}
-            />)})}
+            />)}): ""}
         </TableBody>
       </Table>
 
